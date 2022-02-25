2b2bd2d824320b2f11e781cb101659df8d31e031
tmontaigu <thomas.montaigu@laposte.net>
2022-02-25 19:37:36 +0100

    feat(boolean): make parameters derive Copy
    The `BooleanParameters` is contained `Copy` types, so it
    made sense to also make this type `Copy` by deriving it.
    
    BREAKING CHANGE:
        - The `ClientKey::new` function now takes parameters by value
        and not by reference.

--- concrete-boolean/src/client_key/mod.rs
+++ concrete-boolean/src/client_key/mod.rs
@@ -33,7 +33,7 @@ impl Clone for ClientKey {
         let cks_clone: ClientKey = ClientKey {
             lwe_secret_key: self.lwe_secret_key.clone(),
             glwe_secret_key: self.glwe_secret_key.clone(),
-            parameters: self.parameters.clone(),
+            parameters: self.parameters,
             engine: crate::default_engine(),
         };
         cks_clone
@@ -128,9 +128,9 @@ impl ClientKey {
     /// use concrete_boolean::prelude::*;
     ///
     /// // Generate the client key:
-    /// let cks = ClientKey::new(&DEFAULT_PARAMETERS);
+    /// let cks = ClientKey::new(DEFAULT_PARAMETERS);
     /// ```
-    pub fn new(parameter_set: &BooleanParameters) -> ClientKey {
+    pub fn new(parameter_set: BooleanParameters) -> ClientKey {
         // creation of a core-engine
         let mut engine = crate::default_engine();
 
@@ -148,7 +148,7 @@ impl ClientKey {
         let cks: ClientKey = ClientKey {
             lwe_secret_key,
             glwe_secret_key,
-            parameters: (*parameter_set).clone(),
+            parameters: parameter_set,
             engine,
         };
         cks
