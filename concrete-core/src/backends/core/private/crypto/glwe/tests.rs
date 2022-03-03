1d3e1ed12351955a35f14fa8d4819934a97890f0
Ben <ben.curtis@zama.ai>
2022-03-03 13:44:42 +0000

    feature(core): add tests for the GLWE/Fourier conversion

--- concrete-core/src/backends/core/private/crypto/glwe/tests.rs
+++ concrete-core/src/backends/core/private/crypto/glwe/tests.rs
@@ -1,5 +1,8 @@
+use concrete_commons::dispersion::LogStandardDev;
+
+use crate::backends::core::private::crypto::bootstrap::fourier::buffers::FftBuffers;
 use crate::backends::core::private::crypto::encoding::PlaintextList;
-use crate::backends::core::private::crypto::glwe::GlweList;
+use crate::backends::core::private::crypto::glwe::{FourierGlweCiphertext, GlweList};
 use crate::backends::core::private::crypto::secret::generators::{
     EncryptionRandomGenerator, SecretRandomGenerator,
 };
@@ -8,7 +11,6 @@ use crate::backends::core::private::math::random::RandomGenerator;
 use crate::backends::core::private::math::torus::UnsignedTorus;
 use crate::backends::core::private::test_tools;
 use crate::backends::core::private::test_tools::assert_delta_std_dev;
-use concrete_commons::dispersion::LogStandardDev;
 
 fn test_glwe<T: UnsignedTorus>() {
     // random settings
@@ -36,6 +38,53 @@ fn test_glwe<T: UnsignedTorus>() {
         noise_parameter,
         &mut encryption_generator,
     );
+
+    // decrypts
+    let mut decryptions = PlaintextList::from_tensor(
+        random_generator.random_uniform_tensor(nb_ct.0 * polynomial_size.0),
+    );
+    sk.decrypt_glwe_list(&mut decryptions, &ciphertext);
+
+    // test
+    assert_delta_std_dev(&plaintexts, &decryptions, noise_parameter);
+}
+
+fn test_glwe_fourier_transform<T: UnsignedTorus>() {
+    // random settings
+    let nb_ct = test_tools::random_ciphertext_count(200);
+    let dimension = test_tools::random_glwe_dimension(200);
+    let polynomial_size = test_tools::random_polynomial_size(200);
+    let noise_parameter = LogStandardDev::from_log_standard_dev(-20.);
+    let mut random_generator = RandomGenerator::new(None);
+    let mut secret_generator = SecretRandomGenerator::new(None);
+    let mut encryption_generator = EncryptionRandomGenerator::new(None);
+
+    // generates a secret key
+    let sk = GlweSecretKey::generate_binary(dimension, polynomial_size, &mut secret_generator);
+
+    // generates random plaintexts
+    let plaintexts = PlaintextList::from_tensor(
+        random_generator.random_uniform_tensor(nb_ct.0 * polynomial_size.0),
+    );
+
+    // encrypts
+    let mut ciphertext = GlweList::allocate(T::ZERO, polynomial_size, dimension, nb_ct);
+    sk.encrypt_glwe_list(
+        &mut ciphertext,
+        &plaintexts,
+        noise_parameter,
+        &mut encryption_generator,
+    );
+
+    // transfer to the FFT domain
+
+    let mut fourier_glwe: FourierGlweCiphertext<_, u32> =
+         FourierGlweCiphertext::allocate(Complex64::new(0., 0.),
+                                         PolynomialSize(10),
+                                         GlweSize(7));
+    let buffers_in = &mut buffers.first_buffer;
+    let buffers_out = &mut buffers.second_buffer;
+    // transfer back to the coefficient domain
 
     // decrypts
     let mut decryptions = PlaintextList::from_tensor(
